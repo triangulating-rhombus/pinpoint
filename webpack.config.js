e55e6d73acef02d74d14ab7269678e392c18bf96
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-22 14:48:25 -0800

    Adds gitignore, index.html, index.js, package.json, and webpack.config.js (copied from https://github.com/petehunt/react-webpack-template)

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  cache: true,
+  entry: './index',
+  output: {
+    filename: 'browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+    ]
+  }
+};
