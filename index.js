deb80aea9f8c48256036519a7340fad4a045f271
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-25 12:44:04 -0800

    Deletes deprecated files for vanilla React; updates .gitignore for React Native

--- index.js
+++
@@ -1,7 +0,0 @@
-// Cloned from Pete Hunt's React Webpack Template repo
-// https://github.com/petehunt/react-webpack-template
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'));
