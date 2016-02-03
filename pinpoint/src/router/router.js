3f46ad71d159749b94686de7010614bee5ec336a
Gloria Ma <gloria.ma@aya.yale.edu>
2016-02-03 12:13:57 -0800

    Supposedly works somewhat for login

--- pinpoint/src/router/router.js
+++ pinpoint/src/router/router.js
@@ -7,6 +7,7 @@ import styles from '../styles/styles';
 import Login from '../containers/login-container.js';
 import Signup from '../containers/signup-container.js';
 import Settings from '../containers/settings-container.js';
+import Settings2 from '../containers/settings-container.js';
 import Stats from '../containers/stats-container.js';
 import MapView from '../containers/map-container.js';
 import TabBar from '../components/tabBar.js';
