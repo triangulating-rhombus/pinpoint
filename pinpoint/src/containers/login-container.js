1375f23a0f2cb2e39ef9334a58f9957823a959ab
Gloria Ma <gloria.ma@aya.yale.edu>
2016-02-03 18:58:48 -0800

    Refactors container code for brevity (removes mapDispatchToProps)

--- pinpoint/src/containers/login-container.js
+++ pinpoint/src/containers/login-container.js
@@ -1,21 +1,14 @@
 import { Component } from 'react-native';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import loginUser from '../actions/action_login_user';
 import Login from '../components/login.js';
+import loginUser from '../actions/action_login_user';
 import addSocket from '../actions/action_add_socket';
 
-// Makes user state available to component
 function mapStateToProps({ user, socket }) {
   return { user, socket };
 }
 
-// Makes loginUser action available to component 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({ loginUser, addSocket }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, { loginUser, addSocket })(Login);
 
 
 
