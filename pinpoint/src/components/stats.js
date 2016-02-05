1ad40ba1fef8958ad1c8b3acc8c406940013f91b
Gloria Ma <gloria.ma@aya.yale.edu>
2016-02-04 16:11:20 -0800

    Refactors formatting of chart data to separate function

--- pinpoint/src/components/stats.js
+++ pinpoint/src/components/stats.js
@@ -15,6 +15,23 @@ export default class Stats extends Component {
     this.props.getStats({ lat, lon, tag });
   }
 
+  // Returns this.props.stats in format to be rendered by chart
+  // this.props.stats is an object returned by the server
+  //   of the form: { Sun: 10, Mon: 14, ... }
+  getChartData() {
+    const dayNames = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+    const values = dayNames.map(dayName => this.props.stats[dayName]);
+    return [
+      {
+        name: 'BarChart',
+        type: 'bar',
+        color: '#3369e8',
+        widthPercent: 0.8,
+        data: values
+      }
+    ];
+  }
+
   render() {
     if (Object.keys(this.props.stats).length === 0) {
       return (
@@ -23,33 +40,15 @@ export default class Stats extends Component {
         </View>
       );
     } else {
-      const dataValues = [
-        this.props.stats.Sun,
-        this.props.stats.Mon,
-        this.props.stats.Tues,
-        this.props.stats.Wed,
-        this.props.stats.Thur,
-        this.props.stats.Fri,
-        this.props.stats.Sat
-      ];
-      console.log(dataValues);
-      const chartData = [
-        {
-          name: 'BarChart',
-          type: 'bar',
-          color:'purple',
-          widthPercent: 0.6,
-          data: dataValues //[30, 1, 1, 2, 3, 5, 21, 13, 21, 34, 55, 30],
-        }
-      ];
       return (
         <View style={styles.container}>
           <Text style={styles.formLabel}>By Day</Text>
           <RNChart style={styles.chart}
-            chartData={chartData}
+            chartData={this.getChartData()}
             verticalGridStep={5}
             xLabels={xLabels}
-           />
+            xAxisTitle='Days of Week'
+          />
         </View>
       );
     }
@@ -67,24 +66,9 @@ const styles = StyleSheet.create({
     position: 'absolute', 
     top: 16, 
     left: 4, 
-    bottom: 4,
+    bottom: 30,
     right: 16,
   }
 });
 
-const xLabels = ['0','1','2','3','4','5','6','7','8','9','10','11'];
-
-// export default class Stats extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>HELLO</Text>
-//         <RNChart style={styles.chart}
-//           chartData={chartData}
-//           verticalGridStep={5}
-//           xLabels={xLabels}
-//          />
-//       </View>
-//     );
-//   }
-// }
+const xLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
