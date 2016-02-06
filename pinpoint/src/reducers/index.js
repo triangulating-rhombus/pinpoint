49c0db1ec073a243bed90993d4b6f5f33d1ba0dd
Gloria Ma <gloria.ma@aya.yale.edu>
2016-02-05 19:50:33 -0800

    Removes obsolete action/reducer/etc. files for getStats

--- pinpoint/src/reducers/index.js
+++ pinpoint/src/reducers/index.js
@@ -3,7 +3,6 @@ import UserReducer from './reducer_user';
 import SocketReducer from './reducer_socket';
 import SettingsReducer from './reducer_settings';
 import allUsers from './reducer_updatePins';
-import StatsReducer from './reducer_stats';
 import toggleHotspot from './reducer_toggle_hotspots';
 import PoiReducer from './reducer_poi';
 
@@ -12,7 +11,6 @@ const rootReducer = combineReducers({
   socket: SocketReducer,
   settings: SettingsReducer,
   allUsers: allUsers,
-  stats: StatsReducer,
   hotSpotVisibility: toggleHotspot,
   poi: PoiReducer
 });
