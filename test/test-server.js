ef827a0b744b255e8d70324e161e1d2e22355754
Wayne <Wayne@Jackies-MacBook-Pro.local>
2016-02-06 16:18:54 -0800

    added test

--- test/test-server.js
+++ test/test-server.js
@@ -25,13 +25,24 @@ describe('Test server end points', function() {
     chai.request(server)
       .post('/signup')
       .set('Content-Type', 'application/json')
-      .send({"username": "testtest1", "password": "12345"})
+      .send({"username": "testtest2", "password": "12345"})
       .end(function(err, res){
         console.log(res.body);
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('object');
         res.body.should.have.property('token');
+        done();
+      });
+  });
+
+  it('should be able to get settings', function(done) {
+    chai.request(server)
+      .get('/settings')
+      .set('Content-Type', 'application/json')
+      .set('x-access-token', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.IndheW5lIg.66Qxc0MBJv_cJyvP8WfiEUCZZ4X1BXBSghVQVuAgBTA')
+      .end(function(err, res){
+        console.log(res.body);
         done();
       });
   });
