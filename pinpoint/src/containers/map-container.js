c451c6877601f0d2d5ead3d4899bce58162d067e
Gloria Ma <gloria.ma@aya.yale.edu>
2016-02-10 19:41:38 -0800

    Renames MapView to Map

--- pinpoint/src/containers/map-container.js
+++ pinpoint/src/containers/map-container.js
@@ -1,6 +1,6 @@
 import { Component } from 'react-native';
 import { connect } from 'react-redux';
-import MapView from '../components/mapView';
+import Map from '../components/map';
 import updateStats from '../actions/action_update_stats';
 import setTag from '../actions/action_set_tag';
 
@@ -8,7 +8,7 @@ function mapStateToProps({ user, socket, settings, markers, hotspots, tag }) {
   return { user, socket, settings, markers, hotspots, tag };
 }
 
-export default connect(mapStateToProps, { updateStats, setTag })(MapView);
+export default connect(mapStateToProps, { updateStats, setTag })(Map);
 
 
 
--- pinpoint/src/router/router.js
+++ pinpoint/src/router/router.js
@@ -7,12 +7,11 @@ import styles from '../styles/styles';
 import Login from '../containers/login-container.js';
 import Signup from '../containers/signup-container.js';
 import Settings from '../containers/settings-container.js';
-import Settings2 from '../containers/settings-container.js';
 import Stats from '../containers/stats-container.js';
-import MapView from '../containers/map-container.js';
+import Map from '../containers/map-container.js';
 import TabBar from '../containers/tabBar-container.js';
 
-const ROUTES = { Login, Signup, Settings, Stats, MapView, TabBar };
+const ROUTES = { Login, Signup, Settings, Stats, Map, TabBar };
 
 export default class Router extends Component {
   renderScene(route, navigator) {
